ccea2f723f0acdbf657e3891b3d5246ffb9f0d34
zhuzongxin <dreamsxin@qq.com>
2024-06-15 14:35:48 +0800

    使用 attribute

--- src/core/player.ts
+++ src/core/player.ts
@@ -4,13 +4,13 @@ import { Sprite, Trasition } from "spritejs";
 export class Player extends Sprite {
   private i: number;
   private num: number;
-  private _name: string;
+  //private _name: string;
   private _trans: Trasition;
   constructor(name: string) {
     super(name + "0.png");
     this.i = 0;
     this.num = 8;
-    this._name = name;
+    this.setAttribute("name", name);
     this.attr({
       anchor: [0.6, 0.8],
       size: [250, 220],
@@ -23,11 +23,12 @@ export class Player extends Sprite {
   }
 
   get name() {
-    return this._name;
+    return this.getAttribute("name");
   }
 
   set name(name: string) {
-    this._name = name;
+    this.setAttribute("name", name);
+    //this._name = name;
   }
 
 
@@ -38,12 +39,13 @@ export class Player extends Sprite {
     this.num = 6
     this._trans = this.transition(1.0);
     this._trans.attr({
+      name: "stand0",
       pos: [x, y],
     });
   }
 
   public runloop() {
     this.i = (this.i + 1) % this.num;
-    this.attributes.texture = this.name + this.i + ".png";
+    this.attributes.texture = this.getAttribute("name") + this.i + ".png";
   }
 }
