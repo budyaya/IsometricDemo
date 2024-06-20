f513fa965a1b2ee84ff679153cf9aca65ebafb08
zhuzongxin <dreamsxin@qq.com>
2024-06-20 14:08:27 +0800

    Add stand

---
+++ pixi/stand.js
@@ -0,0 +1,679 @@
+export default {
+  async create() {
+    const standatlasData = {
+      frames: {
+        "stand00.png": {
+          frame: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand01.png": {
+          frame: {
+            x: 250,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand02.png": {
+          frame: {
+            x: 500,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand03.png": {
+          frame: {
+            x: 750,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand04.png": {
+          frame: {
+            x: 1000,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand05.png": {
+          frame: {
+            x: 1250,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand06.png": {
+          frame: {
+            x: 1500,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand07.png": {
+          frame: {
+            x: 1750,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand10.png": {
+          frame: {
+            x: 0,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand11.png": {
+          frame: {
+            x: 250,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand12.png": {
+          frame: {
+            x: 500,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand13.png": {
+          frame: {
+            x: 750,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand14.png": {
+          frame: {
+            x: 1000,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand15.png": {
+          frame: {
+            x: 1250,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand16.png": {
+          frame: {
+            x: 1500,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand17.png": {
+          frame: {
+            x: 1750,
+            y: 220,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand20.png": {
+          frame: {
+            x: 0,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand21.png": {
+          frame: {
+            x: 250,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand22.png": {
+          frame: {
+            x: 500,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand23.png": {
+          frame: {
+            x: 750,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand24.png": {
+          frame: {
+            x: 1000,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand25.png": {
+          frame: {
+            x: 1250,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand26.png": {
+          frame: {
+            x: 1500,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand27.png": {
+          frame: {
+            x: 1750,
+            y: 440,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand30.png": {
+          frame: {
+            x: 0,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand31.png": {
+          frame: {
+            x: 250,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand32.png": {
+          frame: {
+            x: 500,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand33.png": {
+          frame: {
+            x: 750,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand34.png": {
+          frame: {
+            x: 1000,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand35.png": {
+          frame: {
+            x: 1250,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand36.png": {
+          frame: {
+            x: 1500,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+        "stand37.png": {
+          frame: {
+            x: 1750,
+            y: 660,
+            w: 250,
+            h: 220,
+          },
+          rotated: false,
+          trimmed: false,
+          spriteSourceSize: {
+            x: 0,
+            y: 0,
+            w: 250,
+            h: 220,
+          },
+          sourceSize: {
+            w: 250,
+            h: 220,
+          },
+        },
+      },
+      meta: {
+        image: "../assets/character/stand.png",
+        format: "RGBA8888",
+        size: {
+          w: 2000,
+          h: 880,
+        },
+        scale: "1",
+      },
+      animations: {
+        stand: [
+          "stand00.png",
+          "stand01.png",
+          "stand02.png",
+          "stand03.png",
+          "stand04.png",
+          "stand05.png",
+          "stand06.png",
+          "stand07.png",
+        ],
+      },
+    };
+    const standtexture = await PIXI.Assets.load(standatlasData.meta.image);
+    const standspritesheet = new PIXI.Spritesheet(standtexture, standatlasData);
+
+    await standspritesheet.parse();
+
+    const standanim = new PIXI.AnimatedSprite(
+      standspritesheet.animations.stand
+    );
+    standanim.animationSpeed = 0.1666;
+    return standanim;
+  },
+};
